f9620ac76f70cf1bd4ad9d06763fa29f172791a6
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-07-31 20:30:33 -0500

    completely revamped website

---
+++ src/portfolio.js
@@ -0,0 +1,77 @@
+const header = {
+    homepage: '/',
+    title: 'Daniel Nau',
+  }
+  
+  const about = {
+    name: 'Daniel Nau',
+    role: 'Full-Stack Engineer',
+    description:
+      'I strive to make the web an easier, more approachable platform for whatever your requirements are. I also manage to be on time and on budget!',
+    resume: 'https://docs.google.com/document/d/1L7GByLi7Ju_rTywnZlg4q1j40xLCirq-FHIBtZzQWu4/edit?usp=sharing',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/daniel-nau/',
+      github: 'https://github.com/dannauu',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'Weather Dashboard',
+      description:
+        'User can search for a city and get the current weather from Open Weather API.',
+      stack: ['JavaScript', 'HTML', 'CSS'],
+      sourceCode: 'https://github.com/dannauu/Weather-Dashboard',
+      livePreview: 'https://dannauu.github.io/Weather-Dashboard/',
+    },
+    {
+      name: 'Note Taker',
+      description:
+        'Note Taker is a simple application that allows the user to create and delete notes. Built with Nodejs and Expressjs.',
+      stack: ['Javascript', 'CSS', 'HTML'],
+      sourceCode: 'https://github.com/dannauu/Note-Taker',
+      livePreview: 'https://secure-everglades-77184.herokuapp.com/',
+    },
+    {
+      name: 'Employee Management System',
+      description:
+        'Employee Management System is a CLI that allows a company to manage their employees. ',
+      stack: ['Javascript', 'Inquirer', 'MySQL'],
+      sourceCode: 'https://github.com/dannauu/Employee-Management-System',
+   
+    },
+  ]
+  
+  const skills = [
+    'HTML 5',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Material UI',
+    'Git',
+    'Tailwind CSS',
+    'CI/CD',
+    'Jest',
+    'Bootstrap',
+    'Node',
+    'Express',
+    'API Interaction',
+    'jQuery',
+    'MySQL',
+    'MongoDB',
+    'Mongoose',
+    'Visual Studio Code',
+    'Agile',
+    'Postman',
+    'GitLab',
+    'GitHub Pages',
+    'Heroku',
+  
+  ]
+  
+  const contact = {
+    email: 'danielnau1994@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
